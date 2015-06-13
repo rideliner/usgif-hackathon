9a0f80315ba5c71335979244c159cdc5069bcb56
SGTJPai <jpai@sgt-inc.com>
2015-06-13 07:59:54 -0400

    Added custom legend control to show each country's data

--- MapActions.js
+++ MapActions.js
@@ -1,41 +1,59 @@
-
 var geoJSONPath = './geography.json'
 
 var defaultStyle = {
-	fillColor: '#9ecae1',
-	weight: 2,
-	opacity: 1,
-	color: 'white',
-	dashArray: '3',
-	fillOpacity: 0.7
+	fillColor : '#9ecae1',
+	weight : 2,
+	opacity : 1,
+	color : 'white',
+	dashArray : '3',
+	fillOpacity : 0.7
 }
 
 var highlightStyle = {
-	weight: 5,
-	color: '#666',
-	dashArray: '',
-	fillOpacity: 0.7
+	weight : 5,
+	color : '#666',
+	dashArray : '',
+	fillOpacity : 0.7
 }
+
+var info = L.control();
+
+info.onAdd = function(map) {
+	this._div = L.DomUtil.create('div', 'info');
+	this.update();
+	return this._div;
+}
+
+info.update = function(props) {
+	this._div.innerHTML = '<h4>Data Available for Country</h4>'
+			+ (props ? '<b>' + props.NAME + '</b><br />' + props.ECONOMY
+					: 'Hover over a Country');
+}
+
+info.addTo(map);
+
 
 $.getJSON(geoJSONPath, function(data) {
 	L.geoJson(data, {
-		onEachFeature: function(feature, layer) {
+		onEachFeature : function(feature, layer) {
 			layer.on({
-				mouseover: function(e) {
+				mouseover : function(e) {
 					var layer = e.target;
 					layer.setStyle(highlightStyle);
+					info.update(layer.feature.properties);
 					if (!L.Browser.ie && !L.Browser.opera) {
 						layer.bringToFront();
 					}
 				},
-				mouseout: function(e) {
+				mouseout : function(e) {
 					e.target.setStyle(defaultStyle);
+					info.update();
 				},
-				click: function(e) {
+				click : function(e) {
 					map.fitBounds(e.target.getBounds());
 				}
 			})
 		},
-		style: defaultStyle
+		style : defaultStyle
 	}).addTo(map);
 })
