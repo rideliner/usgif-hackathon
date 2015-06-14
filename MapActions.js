ffd167756d6ca8ef2336d529cd3411c2d2f5c321
Nathan Currier <nathan.currier@gmail.com>
2015-06-14 10:31:28 -0400

    Lots of new features and a few refactors.

--- MapActions.js
+++ MapActions.js
@@ -1,11 +1,16 @@
+/**
+ * Code inspired by Leaflet's tutorials. Some code fragments can be found at:
+ * http://leafletjs.com/examples/choropleth.html.
+ */
 var geoJSONPath = './geography.json'
 var airportsJSONPath = './airports.json'
-var ebolaJSONPath = './ebola.json'
+
+
 
 function getColor(d) {
-	return d > 90 ? '#fff7fb' : d > 85 ? '#ece7f2' : d > 80 ? '#d0d1e6'
-			: d > 75 ? '#a6bddb' : d > 70 ? '#74a9cf' : d > 65 ? '#3690c0'
-					: d > 60 ? '#0570b0' : d > 55 ? '#045a8d' : '#023858';
+	return d > 90 ? '#fff5eb' : d > 85 ? '#fee6ce' : d > 80 ? '#fdd0a2'
+			: d > 75 ? '#fdae6b' : d > 70 ? '#fd8d3c' : d > 65 ? '#f16913'
+					: d > 60 ? '#d94801' : d > 55 ? '#a63603' : '#7f2704';
 }
 
 var defaultStyle = {
@@ -17,14 +22,14 @@ var defaultStyle = {
 	fillOpacity : 0
 }
 
-var waterStyle = function(feature){
+var waterStyle = function(feature) {
 	return {
-		fillColor: getColor(feature.properties.PERCENT_ACCESS_TO_CLEAN_WATER),
-		weight: 2,
-		opacity: 1,
-		color: 'white',
-		dashArray: '3',
-		fillOpacity: 0.7
+		fillColor : getColor(feature.properties.PERCENT_ACCESS_TO_CLEAN_WATER),
+		weight : 2,
+		opacity : 1,
+		color : 'white',
+		dashArray : '3',
+		fillOpacity : 0.7
 	}
 }
 
@@ -34,42 +39,44 @@ var highlightStyle = {
 	dashArray : '',
 	fillOpacity : 0.7
 }
-
+/*
 var ZoomReset = L.Control.extend({
-  initialize: function(center, zoom) {
-    L.Util.setOptions(this, {
-      position: 'topleft',
-    });
+	initialize : function(center, zoom) {
+		L.Util.setOptions(this, {
+			position : 'topleft',
+		});
 
-    this._center = center;
-    this._zoom = zoom;
-  },
+		this._center = center;
+		this._zoom = zoom;
+	},
 
-  options: {
-    position: 'topleft'
-  },
+	options : {
+		position : 'topleft'
+	},
 
-  onAdd: function(map) {
-    var container = document.querySelector('.leaflet-control-zoom');
-    var link = L.DomUtil.create('a', 'leaflet-control-zoom-reset', container);
-    link.innerHTML = '↶';
-    link.href = '#';
-    link.title = 'Zoom reset';
+	onAdd : function(map) {
+		var container = document.querySelector('.leaflet-control-zoom');
+		var link = L.DomUtil.create('a', 'leaflet-control-zoom-reset',
+				container);
+		link.innerHTML = '↶';
+		link.href = '#';
+		link.title = 'Zoom reset';
 
-    L.DomEvent
-      .on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
-      .on(link, 'click', L.DomEvent.stop)
-      .on(link, 'click', function() {
-        map.setView(this._center, this._zoom);
-      });
+		L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
+				.on(link, 'click', L.DomEvent.stop).on(link, 'click',
+						function() {
+							map.setView(this._center, this._zoom);
+						});
 
-    return link;
-  }
+		return link;
+	}
 });
 
 map.addControl(new ZoomReset([ 13.364376, -0.3763377 ], 5));
-
-var info = L.control();
+ */
+var info = L.control({
+	position: 'topleft'
+});
 
 info.onAdd = function(map) {
 	this._div = L.DomUtil.create('div', 'info');
@@ -77,33 +84,73 @@ info.onAdd = function(map) {
 	return this._div;
 }
 
+var airports = new L.LayerGroup();
 var numberOfAirports = new Map();
 
 $.getJSON(airportsJSONPath,function(data) {
 	L.geoJson(data,{onEachFeature : function(feature, layer) {
-			layer.bindPopup(feature.properties.name);
-				if (numberOfAirports.get(feature.properties.country) === undefined) {
-					numberOfAirports.set(feature.properties.country,1);
-				} else {
-					numberOfAirports.set(feature.properties.country,numberOfAirports.get(feature.properties.country) + 1);
-				}
-			}
-		}).addTo(airports);
-	})
+		layer.bindPopup(feature.properties.name);
+		if (numberOfAirports.get(feature.properties.country) === undefined) {
+			numberOfAirports.set(feature.properties.country,1);
+		} else {
+			numberOfAirports.set(feature.properties.country,numberOfAirports.get(feature.properties.country) + 1);
+		}
+	} }).addTo(airports);
+})
 
 info.update = function(props) {
 	this._div.innerHTML = '<h4>Data Available for Country</h4>'
 			+ (props ? '<b>' + props.NAME + '</b><br />Number of Airports: '
 					+ numberOfAirports.get(props.NAME)
 					+ '</b><br />Access to clean water: '
-					+ props.PERCENT_ACCESS_TO_CLEAN_WATER + "%"
+					+ props.PERCENT_ACCESS_TO_CLEAN_WATER + "%" 
+					+ '</b><br />Number of Ebola Cases: '
+					+ props.NUMBER_INFECTED
 					: 'Hover over a Country');
 }
-
 info.addTo(map);
 
-$.getJSON(ebolaJSONPath, function(data) {
-	ebola.setData(data);
+var legend = L.control({
+	position : 'bottomleft'
+});
+legend.onAdd = function(map) {
+	var div = L.DomUtil.create('div', 'info legend'), grades = [ 90, 85, 80,
+			75, 70, 65, 60, 55, 0], labels = [], from, to;
+
+	for (var i = 0; i < grades.length - 1; i++) {
+		from = grades[i + 1];
+		to = grades[i];
+
+		labels.push('<i style="background:' + getColor(from + 1) + '"></i> '
+				+ from + (to ? '&ndash;' + to : '&ndash;0'));
+
+	}
+
+	div.innerHTML = '<h4><div style ="text-align:center">% Of People with</div> Access to Clean Water</h4>' + labels.join('<br>');
+	return div;
+}
+legend.addTo(map);
+
+$.getJSON(geoJSONPath, function(data) {
+	L.geoJson(data, {
+		onEachFeature : function(feature, layer) {
+			layer.on({
+				mouseover : function(e) {
+					var layer = e.target;
+					layer.setStyle(highlightStyle);
+					info.update(layer.feature.properties);
+				},
+				mouseout : function(e) {
+					e.target.setStyle(defaultStyle);
+					info.update();
+				},
+				click : function(e) {
+					map.fitBounds(e.target.getBounds());
+				}
+			})
+		},
+		style : defaultStyle
+	}).addTo(defaultLayer);
 })
 
 $.getJSON(geoJSONPath, function(data) {
@@ -114,9 +161,6 @@ $.getJSON(geoJSONPath, function(data) {
 					var layer = e.target;
 					layer.setStyle(highlightStyle);
 					info.update(layer.feature.properties);
-					if (!L.Browser.ie && !L.Browser.opera) {
-						layer.bringToFront();
-					}
 				},
 				mouseout : function(e) {
 					e.target.setStyle(waterStyle(feature));
@@ -128,16 +172,17 @@ $.getJSON(geoJSONPath, function(data) {
 			})
 		},
 		style : waterStyle
-	}).addTo(map)
+	}).addTo(waterLayer);
 })
 
-var colorData = {
-  
-}
 
+var baseMap = {
+	"Default" : defaultLayer,
+	"Water Access" : waterLayer
+}
 var overlay = {
-	"Airports": airports,
-	"Ebola": ebola
+	"Airports" : airports,
+	"Ebola" : ebolaLayer
 }
 
-L.control.layers(colorData, overlay).addTo(map);
+L.control.layers(baseMap, overlay).addTo(map);
